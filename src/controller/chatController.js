ef5d7e239ab2d68663f0ba259dc7369ac4b262f2
JOAO PEDRO DE ARAUJO PEREIRA <joao_pa_pereira@estudante.sc.senai.br>
2024-06-05 20:44:54 -0300

    adicao chat

--- src/controller/chatController.js
+++ src/controller/chatController.js
@@ -1,3 +1,86 @@
 const chatService = require("../service/chat.js");
 
-async function getAllChat(req, res) 
+async function getAllChat(req, res) {
+    try{
+        const rows = await chatService.getAllChat();
+        
+        res.status(200).json(rows);
+    }catch (error){
+        res.status(500).send({
+
+            message: "Error getting chats",
+            body: error.message,
+        });
+    }
+}
+
+async function createChat(req, res) {
+    const { idCreate, mensagemEnviada, mensagemRecebida, dataInteracao } = req.body;
+
+    try{
+        await userController.createChat(idCreate, mensagemEnviada, mensagemRecebida, dataInteracao);
+        
+        res.status(200).json({massege: "Sucess"});
+    }catch (error){
+        res.status(500).send({
+            message: "Error getting chats",
+            body: error.message,
+        });
+    }
+}
+
+async function updateChat(req, res) {
+    try {
+      const { id } = req.params;
+      const { idCreate, mensagemEnviada, mensagemRecebida, dataInteracao } = req.params;
+  
+      await chatService.updateChat (idCreate, mensagemEnviada, mensagemRecebida, dataInteracao);
+  
+      res.status(204).json("Success");
+    } catch (error) {
+      res.status(500).send({
+        message: "Error update user!",
+        error: error.message,
+      });
+    }
+  }
+
+  async function deleteChat(req, res) {
+    try {
+      const { id } = req.params;
+  
+      const chat = await chatService.deleteChat(id);
+  
+      res.status(200).json({ message: "Deleted Chat!" });
+    } catch (error) {
+      res.status(500).send({
+        message: "Error deleting chat!",
+        error: error.message,
+      });
+    }
+  }
+
+  async function getChatById(req, res) {
+    try {
+      const { id } = req.params;
+  
+      const user = await chatService.getChatById(id);
+  
+      res.status(200).json(chat);
+    } catch (error) {
+      res.status(500).send({
+        message: "Error getting chat by ID",
+        error: error.message,
+      });
+    }
+  }
+  
+
+  module.exports = {
+    getAllChat,
+    createChat,
+    updateChat,
+    deleteChat,
+    getChatById,
+  };
+  
